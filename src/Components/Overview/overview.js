90dccaaabdeb7ec8601e78f8c4c56635e425c106
Gian Zamora <gz2016@att.net>
2023-09-26 15:21:52 -0700

    updated recentSearches axios request, the recent searches ui, and web manifest

--- src/Components/Overview/overview.js
+++ src/Components/Overview/overview.js
@@ -1,55 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './overview.css';
 import { RecentSearches } from '../Recent Searches/recentSearches';
 import { Hourly } from '../Hourly/hourly';
 import { Fiveday } from '../5 Day Forecast/fiveday';
 import 'animate.css';
 
-const axios = require('axios'); 
+const axios = require('axios');
 
 export function Overview() {
-    const [forecast, setForecast] = useState(null);
+	const [forecast, setForecast] = useState(null);
 
-    // this callback function receives the searched city entered from recentSearches and applies it to fetchForecast
-    const getSearch = (searchedCity) => {
-        fetchForecast(searchedCity);
-    }; 
+	// this callback function receives the searched city entered from recentSearches and applies it to fetchForecast
+	const getSearch = (searchedCity) => {
+		fetchForecast(searchedCity);
+	};
 
-    async function fetchForecast(searchedCity) {
+	const serverLink = 'https://weatheredd.app/forecast';
 
-        await axios.get(
-            'https://api.weatherapi.com/v1/forecast.json',
-            {params: {
-                q: searchedCity,
-                key: process.env.REACT_APP_API_KEY,
-                days: 3
-            }}
-        ).then((response) => {
-            console.log(response);
-            setForecast(response.data);
-        }).catch((error) => {
-            console.log(error);
-        })
-        
-    };  
-    
-    return (
-        <div>
-            <div className='jumbotron' id='heading-title'>
-                <h1>Welcome to <strong>Weathered</strong>!</h1>
-                <h3>A Simple Weather Dashboard </h3>
-            </div>
+	async function fetchForecast(searchedCity) {
+		await axios
+			.get(serverLink, {
+				params: {
+					q: searchedCity,
+				},
+			})
+			.then((response) => {
+				setForecast(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-            <div className='container-fluid' id='homepage-skeleton'>
-                <div className='d-flex' id='center-page'>
-                    <RecentSearches getSearch={getSearch}/>
+	return (
+		<div>
+			<div className="jumbotron" id="heading-title">
+				<h1>
+					Welcome to <strong>Weathered</strong>!
+				</h1>
+				<h3>A Simple Weather Dashboard </h3>
+			</div>
 
-                    <Hourly forecast={forecast}/>
-                </div>
-            </div>
+			<div className="container-fluid" id="homepage-skeleton">
+				<div className="d-flex" id="center-page">
+					<RecentSearches getSearch={getSearch} />
 
-            <Fiveday forecast={forecast}/>        
-        </div>
-    )
-};
+					<Hourly forecast={forecast} />
+				</div>
+			</div>
 
+			<Fiveday forecast={forecast} />
+		</div>
+	);
+}
--- src/Components/Recent Searches/recentSearches.js
+++ src/Components/Recent Searches/recentSearches.js
@@ -1,68 +1,89 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './recentSearches.css';
 import 'animate.css';
 // import $ from 'jquery';
 
-export function RecentSearches({getSearch}) {
-    const [city, setCity] = useState('');
-    const [recents, setRecents] = useState([]);
+export function RecentSearches({ getSearch }) {
+	const [city, setCity] = useState('');
+	const [recents, setRecents] = useState([]);
+	const [error, setError] = useState('');
 
-    const onSubmit = e => {
-        e.preventDefault();
-        
-        if (!city || city === '') {
-            alert('Please enter a valid city.');
-            return;
-        } else {
-            addRecent(city);
-            getSearch(city);
-        }  
+	const onSubmit = (e) => {
+		e.preventDefault();
 
-    }
- 
-    const onChange = (e) => {
-        setCity(e.target.value);
-    }; 
+		if (!city || city.trim() === '') {
+			setError('Please enter a valid city.');
+			setCity('');
+		} else {
+			addRecent(city);
+			getSearch(city);
+			setError('');
+		}
+	};
 
-    // function which takes in searched entry and adds entry to recent searches array
-    function addRecent(newSearch) {
-        if (recents.includes(newSearch)) {
-            return;
-        } else {
-            setRecents((prev) => [newSearch, ...prev]);
-        }
-        // clear our search bar entry
-        setCity('');
-    }; 
+	const onChange = (e) => {
+		setCity(e.target.value);
+	};
 
-    const searchAgain = (e) => {
-        const recent = e.target.innerHTML;
-        setCity(recent);
-        onSubmit(e);
-    }
+	// function which takes in searched entry and adds entry to recent searches array
+	function addRecent(newSearch) {
+		if (recents.includes(newSearch)) {
+			return;
+		} else {
+			setRecents((prev) => [newSearch, ...prev]);
+		}
+		// clear our search bar entry
+		setCity('');
+	}
 
-    if (recents.length === 0 ) {
-        return (
-            <section id='before-recents' className='before-search-bar-recents' >
-                <form onSubmit={onSubmit} >
-                    <label className="form-label" htmlFor="search-bar">Search A City</label><br/>
-                    <input className='form-text' id="search-bar" type='text' value={city} placeholder="Las Vegas, etc." onChange={onChange}/>
-                    <input className='form-button' id='search-button' type='submit' value='Search'/>
-                </form>
-        </section>
-        )
-    }  
-    
-    return (
-        <section className='after-search-bar-recents animate__animated animate__fadeInLeft'>
-            <h3 className='title'>Recents</h3>
-            <div id='insert-recent-buttons' >{recents.map((entry, index) => <button className='entry' key={index} onClick={searchAgain} >{entry}</button>)}</div>
+	const searchAgain = (e) => {
+		const recent = e.target.innerHTML;
+		setCity(recent);
+		getSearch(recent);
+		setError('');
+	};
 
-            <form className='form'onSubmit={onSubmit} >
-                <label className="form-label" htmlFor="search-bar">Search A City</label>
-                <input className='form-text' id="search-bar" type='text' value={city} placeholder="Las Vegas, etc." onChange={onChange}/>
-                <input className='form-button' id='search-button' type='submit' value='Search'/>
-            </form>
-        </section>
-    )
-};
+	if (recents.length === 0) {
+		return (
+			<section id="before-recents" className="before-search-bar-recents">
+				<form onSubmit={onSubmit}>
+					<label className="form-label" htmlFor="search-bar">
+						Search A City
+					</label>
+					<br />
+					<input
+						className="form-text"
+						id="search-bar"
+						type="text"
+						value={city}
+						placeholder="Las Vegas, etc."
+						onChange={onChange}
+					/>
+					<input className="form-button" id="search-button" type="submit" value="Search" />
+				</form>
+			</section>
+		);
+	}
+
+	return (
+		<section className="after-search-bar-recents animate__animated animate__fadeInLeft">
+			<h3 className="title">Recents</h3>
+			<div id="insert-recent-buttons">
+				{recents.map((entry, index) => (
+					<button className="entry" key={index} onClick={searchAgain}>
+						{entry}
+					</button>
+				))}
+			</div>
+
+			<form className="form" onSubmit={onSubmit}>
+				<label className="form-label" htmlFor="search-bar">
+					Search A City
+				</label>
+				<input className="form-text" id="search-bar" type="text" value={city} placeholder="Las Vegas, etc." onChange={onChange} />
+				<input className="form-button" id="search-button" type="submit" value="Search" />
+			</form>
+			<p className="text-center">{error}</p>
+		</section>
+	);
+}
